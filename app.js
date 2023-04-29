cc2c02e0269f6f8ad2e378796141db4809c42ee2
cbrugman <102334290+cbrugman@users.noreply.github.com>
2023-04-29 13:35:10 -0400

    Got it all working
    added needle to make the API request
    
    This now returns normal JSON that can be read by a browser.

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // These import necessary modules and set some initial variables
 require("dotenv").config();
 const express = require("express");
-const fetch = import("node-fetch");
-const convert = require("xml-js");
+const needle = require('needle');
 const rateLimit = require("express-rate-limit");
 var cors = require("cors");
 const app = express();
 const port = 3000;
 
-// Rate limiting - Goodreads limits to 1/sec, so we should too
+// Rate limiting - Left over from Goodreads version, limits to 1/sec, so we should too
 
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 // see https://expressjs.com/en/guide/behind-proxies.html
@@ -34,28 +33,25 @@ app.get("/", (req, res) => res.send("Hello World!"));
 // My BirdWeather relay route
 app.get("/api/search", async (req, res) => {
   try {
-    // This uses string interpolation to make our search query string
-    // it pulls the posted query param and reformats it for BirdWeather
-    const searchString = `q=${req.query.q}`;
-	console.log(`https://app.birdweather.com/api/v1/stations/{process.env.BIRDWEATHER_API_KEY}&${searchString}`);
-    // It uses node-fetch to call the BirdWeather api, and reads the key from .env
-    const response = await fetch(
-      `https://app.birdweather.com/api/v1/stations/{process.env.BIRDWEATHER_API_KEY}&${searchString}`,
-    );
+    
+	
+    // This pulls the posted query param and reformats it for BirdWeather 
+	// To send a parameter to this search string you use ?q=parameter at the end of the URL 
+	// eg: http://localhost:3000/api/search?q=parameter
+	const searchString = `${req.query.q}`;
+    
+	// Create the api calling url using string interpolation
+	// This also reads the key from .env via dotenv
+	const api_url = `https://app.birdweather.com/api/v1/stations/${process.env.BIRDWEATHER_API_KEY}/${searchString}`
+	
+	// Use needle to call the BirdWeather api
+    	
+	const apiResponse = await needle('get',`${api_url}`);
+    const data = apiResponse.body;
+    res.json(data);
 	
 	//more info here https://app.birdweather.com/api/index.html
-    const results = await response.text();
 
-    // BirdWeather API returns JSON
-    // The API returns stuff we don't care about, so we may as well strip out
-    // everything except the results:
-	//Chris - I'm not sure what the GoodreadsResponse part does here so it's out for now
-    //const results = JSON.parse(json).GoodreadsResponse.search.results;
-
-    return res.json({
-      success: true,
-      results,
-    });
   } catch (err) {
     return res.status(500).json({
       success: false,
